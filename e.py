ea8737f48f338171899e21c2cd87730a78514926
Cristian Bicheru <bichc3017@WRDSB.CA>
2018-05-01 15:36:24 -0400

    initial commit

---
+++ e.py
@@ -0,0 +1,23 @@
+# By Cristian Bicheru
+# Supports Python 3
+# Python 2 Has Some Weird Issues
+############################################
+
+from math import *
+from decimal import *
+
+precision = input()
+
+getcontext().prec = int(precision)
+
+count = -1
+
+e = 0
+
+while True:
+    count += 1
+    d = Decimal(str(e))
+    e = Decimal(str(d)) + Decimal(1 / factorial(count))
+    if e == d:
+        print(str(Decimal(e)))
+        break
